a98dcd2ad070e1eaf37e222b6a51f755dde72025
Binti001 <sharonwafula3@gmail.com>
2022-09-16 22:33:13 -0700

    nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int _putchar(char c)
+{
+	int i;
+
+	i = _putchar(c);
+	return (i);
+	_putchar('\n');
+}
