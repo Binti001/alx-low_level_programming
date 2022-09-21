f09cbddd7e0003b9714c740ce32b5a470c587d63
Binti001 <sharonwafula3@gmail.com>
2022-09-21 09:35:58 -0700

    Hot fix

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *  print_sign - checks if the character is lowercase
+ *  Return: returns 1 and 0 depending on condition
+ *
+ *  @n: a character arguement
+ *
+ *  Returns: 0
+ */
+
+int print_sign(int n)
+{
+	int result;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar(-);
+		return ('-1');
+	}
+}
