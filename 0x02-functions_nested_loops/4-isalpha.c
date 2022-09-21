2fa2072cce72867547188c818696521c8af46c86
Binti001 <sharonwafula3@gmail.com>
2022-09-21 09:11:44 -0700

    isalphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *  _islower - checks if the character is lowercase
+ *  Return: returns 1 and 0 depending on condition
+ *
+ *  @c: a character arguement
+ *
+ *  Returns: 0
+ */
+
+int _islower(int c)
+{
+	int result;
+
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
