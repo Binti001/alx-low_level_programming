d05a74e4df6dc4b89dd131b94c3ab3b1795d6f46
Binti001 <sharonwafula3@gmail.com>
2022-09-21 11:39:24 -0700

    print_last_digits

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - checks if the character is lowercase
+ *  Return: returns 1 and 0 depending on condition
+ *
+ *  @i: a character arguement
+ *
+ *  Returns: 0
+ */
+
+int print_last_digit(int i)
+{
+	i = i % 10;
+		return (i);
+}
