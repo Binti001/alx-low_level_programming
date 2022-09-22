a68881f069002fe59d700469b5638d02981aa136
Binti001 <sharonwafula3@gmail.com>
2022-09-22 02:07:36 -0700

    strncat strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenates the string pointed to @src
+ * to the end of the string pointed by @dest
+ * @dest: first parameter
+ * @src: second parameter
+ * @n: character argument
+ * Return: pointer to a string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int len1;
+	int len2;
+	int i;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+	for (i = 0; i < n; i++)
+	{
+		dest[len1 + i] = src[i];
+	}
+	return (dest);
+}
