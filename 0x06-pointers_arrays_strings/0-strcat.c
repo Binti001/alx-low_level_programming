e84b5ff8cd4f91f44ea060a28df53d061cff5392
Binti001 <sharonwafula3@gmail.com>
2022-09-22 01:49:47 -0700

    concatenate strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,8 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
- * _strcat - concatenates two strings the string pointed to @src
+ * _strcat - concatenates the string pointed to @src
  * to the end of the string pointed by @dest
  * @dest: first parameter
  * @src: second parameter
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	int len1;
 	int len2;
 	int i;
-	
+
 	len1 = strlen(dest);
 	len2 = strlen(src);
 	for (i = 0; i <= len2; i++)
