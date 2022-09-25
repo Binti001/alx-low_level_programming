99383f40d0b795fad74b4fc00ab6583e1e52ad96
Binti001 <sharonwafula3@gmail.com>
2022-09-25 10:47:50 -0700

    add 2 integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @int: parameters
+ * Return: 0
+ */
+
+int add(int, int)
+{
+	int result;
+	int a;
+	int b;
+
+	result = (a + b);
+	return (result);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *  jack_bauer - prints every minute of the day of Jack Bauer
+ *  Return: returns 1 and 0 depending on condition
+ *
+ *  @i: a character arguement
+ *
+ *  Returns: 0
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int a[10][2]; 
+
+	for (i = 0; i < 10; i++)
+	{
+		i = a;
+		_putchar(i);
+	}
+}
