c28354b8b12a24dae2fe018ff3bb2a7cb2fdcc64
Binti001 <sharonwafula3@gmail.com>
2022-10-01 03:48:39 -0700

    Print argc

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: first argument
+ * @argv: second arguement
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
