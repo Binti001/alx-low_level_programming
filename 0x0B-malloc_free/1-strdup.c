c2417e886f0427e0d2ddbed38a049d7487a7185f
Binti001 <sharonwafula3@gmail.com>
2022-10-09 04:10:57 -0700

    Assign memory and duplicate a string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* strdup - returns a pointer to a newly allocated space in memory, which
+* contains a copy of the string given as a parameter
+*
+* @str: string given as a parameter 
+*
+* Return: a pointer to the string
+*/
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i;
+	int j;
+	int result;
+
+	i = 0;
+	j = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+		result = i;
+	}
+	arr = (char *) malloc(sizeof(char) * result);
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	else if (arr == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+	while (str[j] !='\0')
+	{
+		arr[j] = str[j];
+		j++;
+	}
+	}
+	return (arr);
+}
