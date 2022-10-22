d033d4e7cd6b6d2d9ed5fd88bcae98874a66ed45
Binti001 <sharonwafula3@gmail.com>
2022-10-22 03:26:06 -0700

    Turn int into abs using macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(X) ((x < 0) ? ((x) * (-1)) : (x))
+#define ABS(X) ((x < 0) ? (x) * (-1) : (x))
 int _putchar(char c);
 
 #endif
