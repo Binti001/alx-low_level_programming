11393f9ac1d5bb408fa4f14d5c32b84840042abd
Binti001 <sharonwafula3@gmail.com>
2022-10-23 08:34:23 -0700

    Print name,age & owner of dog with a struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog - info about a dog
+* @name: name of dog
+* @age: age of dog
+* @owner: name of dog owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DOG_H_ */
