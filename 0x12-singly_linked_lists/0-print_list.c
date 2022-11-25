e8e2acbb008879fd59e06625d91ceec4de3bd785
Binti001 <sharonwafula3@gmail.com>
2022-11-25 07:43:46 -0800

    lists elements of struct lists_t

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,9 +17,10 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 
-		printf("[0] (nil)\n");
-	else
-		printf("[%ld] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
+		else
+			printf("[%ld] %s\n", h->len, h->str);
+
 		count++;
 		h = h->next;
 	}
