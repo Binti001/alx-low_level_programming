e6a2a5c0fe352e98a8250735280920ad221ff591
Binti001 <sharonwafula3@gmail.com>
2022-12-18 16:09:41 -0800

    insert a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
-		return (add_nodeint_end(head, n));
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	while (temp && count < idx - 1)
 	{
