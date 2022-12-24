d23bce467f36aab85ff5b9ea66bba00e738c1b62
Binti001 <sharonwafula3@gmail.com>
2022-12-23 17:34:45 -0800

    return the number of elements in a doubly linked list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,24 +1,21 @@
 #include "lists.h"
 /**
- * print_dlistint - a function that prints all the elements of
+ * dlistint_len - a function that returns the number of elements of
  * a doubly linked list
  *
  * @h: points to first node
  *
- * Return: the number of nodes
+ * Return: the number of elements
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 	const dlistint_t *head;
 
-	if (h == NULL)
-		return (EXIT_FAILURE);
 	head = h;
 	while (head != NULL)
 	{
-		printf("%d\n", head->n);
 		count++;
 		head = head->next;
 	}
